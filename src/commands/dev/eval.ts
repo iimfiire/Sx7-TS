380b10589d908cebc1216323b5947f7d9c56ecd0
iimfiire <chaseheffner85@gmail.com>
2021-07-06 18:44:45 -0400

    Move file into folder

---
+++ src/commands/dev/eval.ts
@@ -0,0 +1,33 @@
+import Command from '../handler/command';
+
+export default new Command({
+	name: 'eval',
+	category: 'dev',
+	description: 'evalutes a segment of code',
+	info: '',
+	cooldown: 0,
+	minArgs: 1,
+	maxArgs: Infinity,
+	syntax: '<code>',
+	guildOnly: false,
+	devOnly: true,
+	test: false,
+	nsfw: false,
+	noDisable: true,
+	userPerms: ['SEND_MESSAGES'],
+	botPerms: ['ADMINISTRATOR'],
+	execute: ({ message, args, client }) => {
+		try {
+			eval(args.join(' '));
+		} catch (err) {
+			return message.channel.send({
+				embeds: [
+					client.error({
+						message,
+						data: `${err}.`,
+					}),
+				],
+			});
+		}
+	},
+});
