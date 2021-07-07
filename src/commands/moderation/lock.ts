5cd11a7b52990fe9d319b83a25c9456e2b065407
iimfiire <chaseheffner85@gmail.com>
2021-07-06 20:41:23 -0400

    Update lock.ts

--- src/commands/moderation/lock.ts
+++ src/commands/moderation/lock.ts
@@ -2,7 +2,7 @@ import Command from '../../handler/command';
 
 export default new Command({
     name: 'lock',
-    // aliases: [],
+    aliases: ['l'],
     category: 'moderation',
     description: 'Prevents users from typing in a channel by updating permission overwrites for the "everyone" role.',
     info: '',
@@ -12,19 +12,28 @@ export default new Command({
     syntax: '[channel mention or id]',
     guildOnly: true,
     devOnly: false,
-    test: true,
+    test: false,
     nsfw: false,
     noDisable: false,
     userPerms: ['MANAGE_MESSAGES'],
     botPerms: ['MANAGE_CHANNELS', 'SEND_MESSAGES', 'EMBED_LINKS'],
     execute: async ({ message, args, client }) => {
         
-        const channel = args[0] ? await message.guild.channels.fetch(message.mentions.channels.first() ? message.mentions.channels.first().id : typeof BigInt(args[0]) == 'bigint' ? `${BigInt(args[0])}` : message.channel.id) || undefined : message.channel;
+        const channel = args[0] ? await message.guild.channels.fetch(message.mentions.channels.first() ? message.mentions.channels.first().id : !isNaN(parseInt(args[0])) ? `${BigInt(args[0])}` : message.channel.id) : message.channel;
+
+        if(!channel.isText() || channel.type !== 'text') return message.channel.send({embeds: [client.error({message, data: `The provided channel must be a text-channel.`})]});
 
         if(!channel) {
             message.reply({embeds: [client.error({message, data: `Channel not found. You can mention a channel or use a channel id as the first argument when running this command.`})]});
             client.cooldowns.setCooldown(message.author, 'lock', new Date(0));
             return;
-        };
+        }
+        else {
+            if(channel.type == 'text' && channel.isText()) {
+                await channel.permissionOverwrites.create(message.guild.roles.everyone.id, {SEND_MESSAGES: false})
+                return message.reply({embeds: [client.success({message, data: `Locked **${channel.name}**.`})]})
+            }
+        }
     }
 });
+
