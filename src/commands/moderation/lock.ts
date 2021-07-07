7deb403d8ebae3dafb63b317d7182b501de86449
iimfiire <chaseheffner85@gmail.com>
2021-07-06 21:23:54 -0400

    Finish lock/unlock commands

--- src/commands/moderation/lock.ts
+++ src/commands/moderation/lock.ts
@@ -4,7 +4,7 @@ export default new Command({
     name: 'lock',
     aliases: ['l'],
     category: 'moderation',
-    description: 'Prevents users from typing in a channel by updating permission overwrites for the "everyone" role.',
+    description: 'Locks a text-channel, preventing users from sending messages.',
     info: '',
     cooldown: 2000,
     minArgs: 0,
@@ -21,17 +21,17 @@ export default new Command({
         
         const channel = args[0] ? await message.guild.channels.fetch(message.mentions.channels.first() ? message.mentions.channels.first().id : !isNaN(parseInt(args[0])) ? `${BigInt(args[0])}` : message.channel.id) : message.channel;
 
-        if(!channel.isText() || channel.type !== 'text') return message.channel.send({embeds: [client.error({message, data: `The provided channel must be a text-channel.`})]});
+        if(!channel.isText() || channel.type !== 'text') return message.reply({embeds: [client.error({message, data: `The provided channel must be a text-channel.`})], allowedMentions: {repliedUser: false}});
 
         if(!channel) {
-            message.reply({embeds: [client.error({message, data: `Channel not found. You can mention a channel or use a channel id as the first argument when running this command.`})]});
+            message.reply({embeds: [client.error({message, data: `Channel not found. You can mention a channel or use a channel id as the first argument when running this command.`})], allowedMentions: {repliedUser: false}});
             client.cooldowns.setCooldown(message.author, 'lock', new Date(0));
             return;
         }
         else {
             if(channel.type == 'text' && channel.isText()) {
                 await channel.permissionOverwrites.create(message.guild.roles.everyone.id, {SEND_MESSAGES: false})
-                return message.reply({embeds: [client.success({message, data: `Locked **${channel.name}**.`})]})
+                return message.reply({embeds: [client.success({message, data: `Locked **${channel.name}**.`})], allowedMentions: {repliedUser: false}})
             }
         }
     }
--- src/commands/moderation/unlock.ts
+++ src/commands/moderation/unlock.ts
@@ -1,23 +1,42 @@
+import { TextChannel } from 'discord.js';
 import Command from '../../handler/command';
 
 export default new Command({
     name: 'unlock',
     aliases: ['ul'],
     category: 'moderation',
-    description: 'Allows users to send messages to a channel by updating permission overwrites for everyone to allow sending messages.',
+    description: 'Unlocks a previously locked text-channel.',
     info: '',
     cooldown: 2000,
     minArgs: 0,
     maxArgs: Infinity,
-    syntax: '',
-    guildOnly: false,
+    syntax: '[channel mention or id]',
+    guildOnly: true,
     devOnly: false,
     test: true,
     nsfw: false,
     noDisable: false,
-    userPerms: [],
-    botPerms: [],
-    execute: ({ message, args, client }) => {
+    userPerms: ['MANAGE_MESSAGES'],
+    botPerms: ['SEND_MESSAGES', 'MANAGE_CHANNELS', 'EMBED_LINKS'],
+    execute: async ({ message, args, client }) => {
+
+        const channel = args[0] ? await message.guild.channels.fetch(message.mentions.channels.first() ? message.mentions.channels.first().id : !isNaN(parseInt(args[0])) ? `${BigInt(args[0])}` : message.channel.id) : message.channel;
+
+        const isLocked = (channel: TextChannel) => {
+            if(channel.type == 'text') {
+                const perms = channel.permissionsFor(message.guild.roles.everyone)
+                return !perms.has('SEND_MESSAGES')
+            } return false
+        }
+
+        if(channel.isText() && channel.type == 'text') {
+            if(isLocked(channel)) {
+                await channel.permissionOverwrites.edit(message.guild.roles.everyone.id, {SEND_MESSAGES: null})
+                return message.reply({embeds: [client.success({message, data: `Unlocked **${channel.name}**`})]})
+            } else {
+                return message.reply({embeds: [client.error({message, data: `**${channel.name}** is not locked.`})], allowedMentions: {repliedUser: false}})
+            }
+        } else return message.reply({embeds: [client.error({message, data: `**${channel}** is not locked.`})], allowedMentions: {repliedUser: false}})
 
     }
 });
