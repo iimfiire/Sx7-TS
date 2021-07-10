0ea8666c529d0c775844dc604f40521872ee01f3
iimfiire <74388771+iimfiire@users.noreply.github.com>
2021-07-10 19:24:11 -0400

    Delete apis directory

--- apis/Cat-API/Success.js
+++
@@ -1,7 +0,0 @@
-class Success {
-    constructor(data) {
-        this.data = data
-    }
-}
-
-module.exports = Success
--- apis/Typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { RaidenCol } from "./collection";
-
-export declare function test(id: string): RaidenCol<string, MemeBeautify>
-export declare function test(): string
