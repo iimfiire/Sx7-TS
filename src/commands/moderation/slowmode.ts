3abfa0b199f6cfe5d043e16d2d3544fd3c3c316b
iimfiire <chaseheffner85@gmail.com>
2021-07-09 21:37:11 -0400

    formatting, bugfixing

--- src/commands/moderation/slowmode.ts
+++ src/commands/moderation/slowmode.ts
@@ -8,7 +8,7 @@ export default new Command({
 	description: 'Sets the slowmode in a channel.',
 	info: '',
 	cooldown: 2500,
-	minArgs: 1,
+	minArgs: 0,
 	maxArgs: Infinity,
 	syntax: '[channel/time] [time]',
 	guildOnly: true,
@@ -18,7 +18,7 @@ export default new Command({
 	noDisable: false,
 	userPerms: ['MANAGE_MESSAGES'],
 	botPerms: ['MANAGE_CHANNELS', 'SEND_MESSAGES', 'EMBED_LINKS'],
-	execute: async ({ message, args, client }) => {
+	execute: async ({ message, args, client, prefix }) => {
 		const channel = args[1]
 			? await message.guild.channels.fetch(
 					message.mentions.channels.first()
@@ -28,12 +28,56 @@ export default new Command({
 						: message.channel.id
 			  )
 			: message.channel;
-		const time = args[1] ? ms(args[1]) : ms(args[0]);
-		if (isNaN(time)) return;
+
+		const time = args[1]
+			? ms(args[1])
+			: args[0]
+			? isNaN(parseInt(args[0]))
+				? ms(args[0])
+				: parseInt(args[0]) * 1000
+			: 0;
+
+		if (isNaN(time))
+			return message.reply({
+				embeds: [
+					client.error({
+						message,
+						data: `An invalid time was provided. A valid time should look like \`${prefix}slowmode 2s\``,
+					}),
+				],
+				allowedMentions: { repliedUser: false },
+			});
 
 		if (channel.type == 'text') {
 			channel.setRateLimitPerUser(time / 1000);
-			message.reply('set slowmode');
-		} else return;
+			if (time == 0)
+				return message.reply({
+					embeds: [
+						client.success({
+							message,
+							data: `Removed the slowmode from **${channel.name}**.`,
+						}),
+					],
+					allowedMentions: { repliedUser: false },
+				});
+			return message.reply({
+				embeds: [
+					client.success({
+						message,
+						data: `Set the slowmode in **${channel.name}** to **${ms(time)}**.`,
+					}),
+				],
+				allowedMentions: { repliedUser: false },
+			});
+		} else
+			return message.reply({
+				embeds: [
+					client.error({
+						message,
+						data: `A slowmode cannot be set in **${channel}**.`,
+					}),
+				],
+				allowedMentions: { repliedUser: false },
+			});
 	},
 });
